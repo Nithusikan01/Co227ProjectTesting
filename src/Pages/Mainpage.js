7cfc6cb2a1839ebb055037ca9bceb7fcddf79f35
Nithusikan <e19266@eng.pdn.ac.lk>
2023-09-20 01:20:21 +0530

    footer is created

--- src/Pages/Mainpage.js
+++ src/Pages/Mainpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import './MainPage.css';
 import AppBar from "../components/Appbar/Appbar";
 import TitleBar from "../components/Titlebar/Titlebar.js";
@@ -9,38 +9,60 @@ import ContactUs from "./ContactUs/ContactUs";
 import Development from "../Pages/Development/Development";
 import PresentStaff from "../Pages/Staff/presentStaff";
 import PastStaff from "../Pages/Staff/PastStaff";
-import AddPresentStaff from "../Pages/AddPresenStaff";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import MyForm from "./Staff/FomDataAdd";
 import PresentStaffNew from "./Staff/presentStaffNew";
 import PastStaffNew from "./Staff/pastStaffNew";
 import GES from "../Pages/4_General_Elective_Subjects/Front-End/GES";
 import PastStaffForm from "./Staff/pastStaffForm";
+import Footer from "../components/Footer/footer";
 
 function MainPage() {
+    const [contentHeight, setContentHeight] = useState(0);
 
-    return (
-      
+    useEffect(() => {
+        const handleResize = () => {
+        setContentHeight(document.documentElement.scrollHeight);
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize(); // Initial adjustment on page load
+
+        return () => {
+        window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return (  
     <>
-    <BrowserRouter>
-        <TitleBar/>
-        <AppBar/>
-        <SideBar/>
-        <Routes>
+        <div className="content">
+            <BrowserRouter>
+                <TitleBar/>
+                <AppBar/>
+                <SideBar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/About EEU" element={<About/>}/>
+                    <Route path="/ContactUs" element={<ContactUs/>}/>
+                    <Route path="/Development" element={<Development/>}/>
+                    <Route path="/PresentStaff" element={<PresentStaff/>}/>
+                    <Route path="/PastStaff" element={<PastStaff/>}/>
+                    <Route path="/MyForm" element={<MyForm/>}/>
+                    <Route path="/PresentStaffNew" element={<PresentStaffNew/>}/>
+                    <Route path="/PastStaffNew" element={<PastStaffNew/>}/>
+                    <Route path="/GES" element={<GES/>}/>
+                    <Route path="/PastStaffForm" element={<PastStaffForm/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
 
-            <Route path="/" element={<Home/>}/>
-            <Route path="/About EEU" element={<About/>}/>
-            <Route path="/ContactUs" element={<ContactUs/>}/>
-            <Route path="/Development" element={<Development/>}/>
-            <Route path="/PresentStaff" element={<PresentStaff/>}/>
-            <Route path="/PastStaff" element={<PastStaff/>}/>
-            <Route path="/MyForm" element={<MyForm/>}/>
-            <Route path="/PresentStaffNew" element={<PresentStaffNew/>}/>
-            <Route path="/PastStaffNew" element={<PastStaffNew/>}/>
-            <Route path="/GES" element={<GES/>}/>
-            <Route path="/PastStaffForm" element={<PastStaffForm/>}/>
-        </Routes>
-    </BrowserRouter>
+        <div
+            className="footer"
+            style={{
+                position: contentHeight < window.innerHeight ? "absolute" : "static"
+            }}
+        >
+        <Footer />
+        </div>
     </>
   
     );
--- src/Pages/Staff/pastStaffNew.js
+++ src/Pages/Staff/pastStaffNew.js
@@ -14,25 +14,50 @@ const PastStaffNew = () => {
       const response = await axios.get("http://localhost:8080/pastStaff/all");
       setPastStaffs(response.data);
     } catch (error) {
-      console.error("Error loading present staffs:", error);
+      console.error("Error loading past staffs:", error);
     }
   };
 
-  const directors = pastStaffs.filter((pastStaff) =>pastStaff.roleOnEEUWebsite === "Director");
+  const directors = pastStaffs.filter(
+    (pastStaff) => pastStaff.roleOnEEUWebsite === "Director"
+  );
+
+  const otherStaffs = pastStaffs.filter(
+    (pastStaff) => pastStaff.roleOnEEUWebsite !== "Director"
+  );
 
   return (
-    <div className="director-list-container">
-      <div className="director-list">
-      {directors.map((director, index) => (
-        <div className="director-card" key={index}>
-          <img src={director.image} alt="Image is missing" />
-          <div>
-            <h2>Name: {director.name}</h2>
-            <p>Qualifications: {director.period}</p>
-            <p>Role on EEU Website: {director.roleOnEEUWebsite}</p>
-          </div>
+    <div className="pastStaff-container" style={{alignItems: "center",justifyContent: "center"}}>
+      <div className="director-list-container">
+        <h2>Directors</h2>
+        <div className="director-list">
+          {directors.map((director, index) => (
+            <div className="director-card" key={index}>
+              <img src={director.image} alt="Image is missing" />
+              <div>
+                <h2>Name: {director.name}</h2>
+                <p>Qualifications: {director.period}</p>
+                <p>Role on EEU Website: {director.roleOnEEUWebsite}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
+
+      <div className="other-staff-list-container">
+        <h2>Other Staff Members</h2>
+        <div className="other-staff-list">
+          {otherStaffs.map((staff, index) => (
+            <div className="staff-card" key={index}>
+              <img src={staff.image} alt="Image is missing" />
+              <div>
+                <h2>Name: {staff.name}</h2>
+                <p>Qualifications: {staff.period}</p>
+                <p>Role on EEU Website: {staff.roleOnEEUWebsite}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
